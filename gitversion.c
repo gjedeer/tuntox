3adc626eb1646ee3456ca282e1855142f9f935e0
GDR! <gdr@gdr.name>
2018-10-25 11:39:47 +0200

    Print toxcore version in debug output
    Signed-off-by: GDR! <gdr@gdr.name>

--- gitversion.c
+++ gitversion.c
@@ -5,5 +5,5 @@ const char *gitversion = GITVERSION;
 
 void print_version()
 {
-    log_printf(L_INFO, "Tuntox built from git commit %s", gitversion);
+    log_printf(L_INFO, "Tuntox built from git commit %s, toxcore version %d.%d.%d", gitversion, tox_version_major(), tox_version_minor(), tox_version_patch());
 }
