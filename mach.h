1dce354e499ce6a223b88d42909fa3608caefb6e
Will Cohen <willcohen@users.noreply.github.com>
2022-02-17 20:23:59 -0500

    Fix mach on macOS

--- mach.h
+++ mach.h
@@ -4,10 +4,6 @@
 #include <time.h>
 #include <mach/mach_time.h>
 
-// there is no CLOCK_REALTIME or CLOCK_MONOTONIC on MacOS platform
-#define CLOCK_REALTIME 0
-#define CLOCK_MONOTONIC 0
-
 // MacOS doesn't support the flag MSG_NOSIGNAL
 #define MSG_NOSIGNAL SO_NOSIGPIPE
 
