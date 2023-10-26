1f3cb8e35d37333f34b7deba636b80cb25b24801
GDR! <gdr@gdr.name>
2023-10-26 21:20:52 +0200

    Inform about proto version on startup

--- gitversion.c
+++ gitversion.c
@@ -6,7 +6,7 @@ const char *gitversion = GITVERSION;
 
 void print_version()
 {
-    log_printf(L_INFO, "Tuntox built from git commit %s, toxcore version %d.%d.%d", gitversion, tox_version_major(), tox_version_minor(), tox_version_patch());
+    log_printf(L_INFO, "Tuntox built from git commit %s, toxcore version %d.%d.%d, protocol version 2", gitversion, tox_version_major(), tox_version_minor(), tox_version_patch());
 }
 
 void print_version_stdout()
